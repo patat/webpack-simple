0a5fd387250af626850d564f7003ea6f58839276
Patat <nemnogosloven@gmail.com>
2017-02-01 22:30:07 +0300

    add support for fonts, images && jade

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import normalize from 'normalize.css'
+import scriptinaRegular from './fonts/scriptina_regular/stylesheet.css';
 import styles from './css/app.css';
 
 import './js/app';
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const extractCSS = new ExtractTextPlugin('styles/[name].css');
+const extractCSS = new ExtractTextPlugin('[name].css');
 
 module.exports = {
   devtool: "cheap-eval-source-map",
@@ -12,7 +13,8 @@ module.exports = {
 
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
 
   module: {
@@ -22,23 +24,99 @@ module.exports = {
         use: extractCSS.extract({
           loader: 'css-loader?importLoaders=1!postcss-loader'
         }),
-        include: path.resolve(__dirname, 'src')
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve(__dirname, 'node_modules/normalize.css')
+        ]
       },
       {
         test: /\.js$/,
         use: [ 'babel-loader' ],
         include: path.resolve(__dirname, 'src')
+      }, 
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=fonts/[name].[ext]',
+        include: path.resolve(__dirname, 'src/fonts')
+      },
+      {
+        test: /\.(jpe?g|png|gif|(?!svg))$/i, 
+        loader: "file-loader?name=img/[name].[ext]",
+        include: path.resolve(__dirname, 'src/img')
+      },
+      { 
+        test: /\.jade$/, 
+        loader: 'jade-loader?pretty' 
       }
+      // {
+      //   test: /\.html$/,
+      //   loader: 'file-loader?name=[path][name].[ext]!extract-loader!html-loader',
+      //   include: path.resolve(__dirname, 'src')
+      // }
     ]
   },
 
   plugins: [
-    extractCSS
+    extractCSS,
+    new HtmlWebpackPlugin({
+      // Required
+      inject: false,
+      template: 'src/index.template.jade',
+      devServer: 'http://localhost:8080',
+      title: 'My App',
+
+      // Optional
+      // appMountId: 'app',
+      // baseHref: 'http://example.com/awesome',
+      
+      // googleAnalytics: {
+      //   trackingId: 'UA-XXXX-XX',
+      //   pageViewOnLoad: true
+      // },
+      // meta: [
+      //   {
+      //     name: 'description',
+      //     content: 'A better default template for html-webpack-plugin.'
+      //   }
+      // ],
+      // mobile: true,
+      // links: [
+      //   'https://fonts.googleapis.com/css?family=Roboto',
+      //   {
+      //     href: '/apple-touch-icon.png',
+      //     rel: 'apple-touch-icon',
+      //     sizes: '180x180'
+      //   },
+      //   {
+      //     href: '/favicon-32x32.png',
+      //     rel: 'icon',
+      //     sizes: '32x32',
+      //     type: 'image/png'
+      //   }
+      // ],
+      // inlineManifestWebpackName: 'webpackManifest',
+      // scripts: [
+      //   'http://example.com/somescript.js',
+      //   {
+      //     src: '/myModule.js',
+      //     type: 'module'
+      //   }
+      // ],
+      
+      // window: {
+      //   env: {
+      //     apiHost: 'http://myapi.com/api/v1'
+      //   }
+      // }
+
+      // And any other config options from html-webpack-plugin:
+      // https://github.com/ampedandwired/html-webpack-plugin#configuration
+    })
   ],
 
   devServer: {
-    contentBase: './',
+    contentBase: '/',
     watchContentBase: true,
-    publicPath: "/dist/"
+    publicPath: '/'
   }
 };
